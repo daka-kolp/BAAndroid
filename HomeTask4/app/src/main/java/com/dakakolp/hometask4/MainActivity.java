bc51ea9b55efff6a5dc502941eaa38e622ff4ffb
daka-kolp <daka.kolp01@gmail.com>
2018-08-19 14:50:43 +0300

    HomeTask4: AdapterView and RecycleView (finished)

--- HomeTask4/app/src/main/java/com/dakakolp/hometask4/MainActivity.java
+++ HomeTask4/app/src/main/java/com/dakakolp/hometask4/MainActivity.java
@@ -2,12 +2,33 @@ package com.dakakolp.hometask4;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private ProductAdapter productAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.main_list);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        List<Product> products = new ArrayList<Product>();
+
+        products.add(new Product(R.drawable.if_apple1, "Green Watch", 200.50, "Apple's green modern watch, create your own style..."));
+        products.add(new Product(R.drawable.if_apple2, "White Watch", 150.50, "Apple's modern watch, create your new style..."));
+        products.add(new Product(R.drawable.if_apple3, "Apple's Laptop", 1000.75, "Apple's laptop, change your life..."));
+
+
+        productAdapter = new ProductAdapter(products);
+        recyclerView.setAdapter(productAdapter);
     }
 }
--- HomeTask4/app/src/main/java/com/dakakolp/hometask4/ProductAdapter.java
+++ HomeTask4/app/src/main/java/com/dakakolp/hometask4/ProductAdapter.java
@@ -5,30 +5,60 @@ import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.LayoutInflater;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.ProductViewHolder>{
+import java.util.Formatter;
+import java.util.List;
+
+public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.ProductViewHolder> {
+
+
+    private List<Product> productsList;
+
+    public ProductAdapter(List<Product> productsList) {
+        this.productsList = productsList;
+    }
 
     @NonNull
     @Override
     public ProductViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         return new ProductViewHolder(
-                LayoutInflater.from(parent.getContext()).inflate(R.layout.list_products, null));
+                LayoutInflater
+                        .from(parent.getContext())
+                        .inflate(R.layout.list_products, null));
     }
 
     @Override
     public void onBindViewHolder(@NonNull ProductViewHolder holder, int position) {
+        Product product = productsList.get(position);
+        holder.productImage.setImageResource(product.getImage());
+        holder.productNameTxt.setText(product.getName());
+        holder.productPriceTxt.setText(String.format("%.2f", product.getPrice()));
+        holder.productDescriptionTxt.setText(product.getDescription());
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return productsList.size();
     }
 
-    class ProductViewHolder extends RecyclerView.ViewHolder{
+
+    class ProductViewHolder extends RecyclerView.ViewHolder {
+
+        private ImageView productImage;
+        private TextView productNameTxt;
+        private TextView productPriceTxt;
+        private TextView productDescriptionTxt;
+
 
         public ProductViewHolder(View itemView) {
             super(itemView);
+            productImage = itemView.findViewById(R.id.product_image);
+            productNameTxt = itemView.findViewById(R.id.product_name);
+            productPriceTxt = itemView.findViewById(R.id.product_price);
+            productDescriptionTxt = itemView.findViewById(R.id.product_description);
         }
     }
 }
