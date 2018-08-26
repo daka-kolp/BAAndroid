40ed327c89929f3f5a13748f76f027f81e7900e8
daka-kolp <daka.kolp01@gmail.com>
2018-08-26 18:21:06 +0300

    HomeTask5: CoordinatorLayout and FloatingActionButton with SnackBar

--- HomeTask5/app/src/main/java/com/dakakolp/hometask5/IngredientsActivity.java
+++ HomeTask5/app/src/main/java/com/dakakolp/hometask5/IngredientsActivity.java
@@ -1,6 +1,8 @@
 package com.dakakolp.hometask5;
 
 import android.content.Intent;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -15,13 +17,13 @@ public class IngredientsActivity extends AppCompatActivity {
     public static final String RECIPE_ID = "recipeID";
     public static final String LIST_RECIPES = "listRecipes";
 
-    ArrayList<Product> list;
-    int positionId;
+    private ArrayList<Product> list;
+    private int positionId;
 
-    TextView nameTV;
-    TextView ingredientsTV;
-    ImageView photoIV;
-    View ingredientsView;
+    private TextView nameTV;
+    private TextView ingredientsTV;
+    private ImageView photoIV;
+    private FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,8 +45,8 @@ public class IngredientsActivity extends AppCompatActivity {
         photoIV.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ingredientsView = findViewById(R.id.linear_ingredients);
-                Snackbar sb = Snackbar.make(ingredientsView, "Show image?", Snackbar.LENGTH_LONG);
+                fab = findViewById(R.id.fab);
+                Snackbar sb = Snackbar.make(fab, "Show image?", Snackbar.LENGTH_LONG);
                 sb.show();
                 sb.setAction("Display", new View.OnClickListener() {
                     @Override
